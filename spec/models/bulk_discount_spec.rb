25dc18844af90f39b6a16f88f432f2a56da1bc94
Joe King <j.king92@gmail.com>
2023-01-12 15:53:31 -0700

    Add validations to bulk discount

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -4,4 +4,13 @@ RSpec.describe BulkDiscount do
   describe 'relationships' do
     it {should belong_to :merchant}
   end
+
+  describe 'validations' do
+    it {should validate_numericality_of :discount}
+    it {should validate_numericality_of :qty_threshold}
+  end
+
+  it '' do
+    require 'pry'; binding.pry
+  end
 end
