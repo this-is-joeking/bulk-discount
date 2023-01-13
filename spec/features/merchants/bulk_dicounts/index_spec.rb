4733b2f8a14f8042f3b5545e3307d2ea828fac5f
Joe King <j.king92@gmail.com>
2023-01-13 15:14:46 -0700

    Add routes, and link for new discount from merchant discount index page

--- spec/features/merchants/bulk_dicounts/index_spec.rb
+++ spec/features/merchants/bulk_dicounts/index_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'merchant/bulk discounts index page' do
     @bd1 = @merchant1.bulk_discounts.create!(discount: 99, qty_threshold: 1000)
     @bd2 = @merchant1.bulk_discounts.create!(discount: 18, qty_threshold: 15)
     @bd3 = @merchant1.bulk_discounts.create!(discount: 20, qty_threshold: 25)
-    @bd4 = @merchant2.bulk_discounts.create!(discount: 1, qty_threshold: 2)
+    @bd4 = @merchant2.bulk_discounts.create!(discount: 11, qty_threshold: 7)
   end
 
   it 'lists all discounts that belong to the merchant and a link to the discounts show page' do
@@ -20,6 +20,22 @@ RSpec.describe 'merchant/bulk discounts index page' do
         expect(page).to have_link("View", href: merchant_bulk_discount_path(@merchant1, bd))
       end
     end
+    expect(page).to_not have_content(@bd4.discount)
+    expect(page).to_not have_content(@bd4.qty_threshold)
+  end
+
+  it 'has a link to create a new bulk discount' do
+    visit merchant_bulk_discounts_path(@merchant1)
+
+    expect(page).to have_link("Create a New Bulk Discount", href: new_merchant_bulk_discount_path(@merchant1))
   end
 
 end
+
+# As a merchant When I visit my bulk discounts index 
+# Then I see a link to create a new discount 
+# When I click this link Then I am taken to a new page where 
+# I see a form to add a new bulk discount 
+# When I fill in the form with valid data 
+# Then I am redirected back to the bulk discount index 
+# And I see my new bulk discount listed 
