81d2ee9b89c679bf044fad21bc5c284b29347edd
Joe King <j.king92@gmail.com>
2023-01-13 20:42:02 -0700

    Complete us4- Add bulk discount show page

---
+++ spec/features/merchants/bulk_dicounts/show_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'merchants bulk discount show page' do
+  before(:each) do
+    @merchant1 = Merchant.find(1)
+    @bd1 = @merchant1.bulk_discounts.create!(discount: 25, qty_threshold: 100)
+  end
+
+  it 'lists the discount percentage and quantity threshold' do
+    visit merchant_bulk_discount_path(@merchant1, @bd1)
+
+    expect(page).to have_content("Discount: #{@bd1.discount}%")
+    expect(page).to have_content("Quantity threshold for discount to apply: #{@bd1.qty_threshold} items")
+  end
+end
