fe737d9003af2d7b2fe077cfe568884b3dea9397
Joe King <j.king92@gmail.com>
2023-01-17 11:44:19 -0700

    Add relationships and tests to bulk_discount

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe BulkDiscount do
   describe 'relationships' do
     it {should belong_to :merchant}
+    it {should have_many(:items).through(:merchant)}
+    it {should have_many(:invoice_items).through(:items)}
   end
 
   describe 'validations' do
